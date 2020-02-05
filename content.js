3c37a9906ce8fc87d0cf622742f932c892e2d56a
Matt Beachey <mattbeachey@gmail.com>
2020-02-04 20:33:03 -0600

    chrome extension gets videos current time

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 // alert("alert")
 
+const videoEl = document.querySelector("video")
+
 chrome.runtime.onMessage.addListener(function (req) {
     alert(req)
+    console.log(videoEl.currentTime)
 })
