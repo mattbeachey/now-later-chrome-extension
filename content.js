a78c1804ac2426fb5b8127bba9005b40c79e28c7
Matt Beachey <mattbeachey@gmail.com>
2020-02-04 21:33:44 -0600

    extension creates current timestamp url with click

--- content.js
+++ content.js
@@ -2,7 +2,18 @@
 
 const videoEl = document.querySelector("video")
 
-chrome.runtime.onMessage.addListener(function (req) {
-    alert(req)
-    console.log(videoEl.currentTime)
+chrome.runtime.onMessage.addListener(function (videoUrl) {
+
+    if (videoUrl.includes("&")) {
+        const videoId = videoUrl.split("?v=").pop()
+        const newVideoId = videoId.split("&").shift();
+        const timeElapsed = (Math.floor(videoEl.currentTime))
+        const timestampUrl = ("https://youtu.be/" + newVideoId + "?t=" + timeElapsed)
+        alert(timestampUrl)
+    } else {
+        const videoId = videoUrl.split("?v=").pop()
+        const timeElapsed = (Math.floor(videoEl.currentTime))
+        const timestampUrl = ("https://youtu.be/" + videoId + "?t=" + timeElapsed)
+        alert(timestampUrl)
+    }
 })
--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("button").addEventListener("click", testFunction)
-    
+
     function testFunction() {
         chrome.tabs.query({ currentWindow: true, active: true },
             function (tabs) { 
-                chrome.tabs.sendMessage(tabs[0].id, "hello")
+                const currentUrl = tabs[0].url;
+                chrome.tabs.sendMessage(tabs[0].id, currentUrl)
             })
     }
 })
