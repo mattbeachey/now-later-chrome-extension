545ae7c54e703d05a0310ea60d83a2846db27729
Matt Beachey <mattbeachey@gmail.com>
2020-02-08 19:18:18 -0600

    user can add multiple tags comma seperated

--- content.js
+++ content.js
@@ -5,26 +5,30 @@ const videoEl = document.querySelector("video")
 chrome.runtime.onMessage.addListener(function (videoUrl) {
 
     //differentiates between types of youtube urls to extract video id from full url
-    if (videoUrl.includes("&")) {
-        const videoId = videoUrl.split("?v=").pop()
-        const newVideoId = videoId.split("&").shift();
-        const timeElapsed = (Math.floor(videoEl.currentTime))
-        const timestampUrl = ("https://youtu.be/" + newVideoId + "&t=" + timeElapsed)
-        // alert(timestampUrl)
-        chrome.runtime.sendMessage({
-            data: ("http://localhost:3000/add?url=" + timestampUrl + "&data1=value" + "&data2=value")
-        }, function (response) {
-            console.dir(response);
-        });
+    if (videoUrl.includes("youtube")){
+        if (videoUrl.includes("&")) {
+            const videoId = videoUrl.split("?v=").pop()
+            const newVideoId = videoId.split("&").shift();
+            const timeElapsed = (Math.floor(videoEl.currentTime))
+            const timestampUrl = ("https://youtu.be/" + newVideoId + "&t=" + timeElapsed)
+            // alert(timestampUrl)
+            chrome.runtime.sendMessage({
+                data: ("http://localhost:3000/add?url=" + timestampUrl + "&data1=value, value, value" + "&data2=value")
+            }, function (response) {
+                console.dir(response);
+            });
+        } else {
+            const videoId = videoUrl.split("?v=").pop()
+            const timeElapsed = (Math.floor(videoEl.currentTime))
+            const timestampUrl = ("https://youtu.be/" + videoId + "&t=" + timeElapsed)
+            // alert(timestampUrl)
+            chrome.runtime.sendMessage({
+                data: ("http://localhost:3000/add?url=" + timestampUrl  + "&data1=value, value, value" + "&data2=value")
+            }, function (response) {
+                console.dir(response);
+            });
+        }
     } else {
-        const videoId = videoUrl.split("?v=").pop()
-        const timeElapsed = (Math.floor(videoEl.currentTime))
-        const timestampUrl = ("https://youtu.be/" + videoId + "&t=" + timeElapsed)
-        // alert(timestampUrl)
-        chrome.runtime.sendMessage({
-            data: ("http://localhost:3000/add?url=" + timestampUrl  + "&data1=value" + "&data2=value")
-        }, function (response) {
-            console.dir(response);
-        });
+            alert("This plugin only works with Youtube currently!")
     }
 })
