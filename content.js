7da0e841228f48141dda2d06d12ce63daddbc1a7
Matt Beachey <mattbeachey@gmail.com>
2020-02-11 17:10:57 -0600

    gets original video title and passes to front end

--- content.js
+++ content.js
@@ -1,11 +1,14 @@
 
 const videoEl = document.querySelector("video")
+const videoName = document.title.split(" - YouTube")[0]
+
+console.log("TITLE::::::::::::::::::::::::::::::::::::: " + videoName)
 
 //listener for query from popup.js
 chrome.runtime.onMessage.addListener(function (videoUrl) {
 
     //differentiates between types of youtube urls to extract video id from full url
-    if (videoUrl.includes("youtube")){
+    if (videoUrl.includes("youtube")) {
         if (videoUrl.includes("&")) {
             const videoId = videoUrl.split("?v=").pop()
             const newVideoId = videoId.split("&").shift();
@@ -13,7 +16,7 @@ chrome.runtime.onMessage.addListener(function (videoUrl) {
             const timestampUrl = ("https://youtu.be/" + newVideoId + "&t=" + timeElapsed)
             // alert(timestampUrl)
             chrome.runtime.sendMessage({
-                data: ("http://localhost:3000/add?url=" + timestampUrl + "&data1=value, value, value" + "&data2=value")
+                data: ("http://localhost:3000/add?url=" + timestampUrl + "&videoName=" + videoName + "&data1=value, value, value" + "&data2=value")
             }, function (response) {
                 console.dir(response);
             });
@@ -23,12 +26,12 @@ chrome.runtime.onMessage.addListener(function (videoUrl) {
             const timestampUrl = ("https://youtu.be/" + videoId + "&t=" + timeElapsed)
             // alert(timestampUrl)
             chrome.runtime.sendMessage({
-                data: ("http://localhost:3000/add?url=" + timestampUrl  + "&data1=value, value, value" + "&data2=value")
+                data: ("http://localhost:3000/add?url=" + timestampUrl + "&videoName=" + videoName + "&data1=value, value, value" + "&data2=value")
             }, function (response) {
                 console.dir(response);
             });
         }
     } else {
-            alert("This plugin only works with Youtube currently!")
+        alert("This plugin only works with Youtube currently!")
     }
 })
